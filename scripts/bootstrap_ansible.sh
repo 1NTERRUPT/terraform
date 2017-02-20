b21197074b58f764a24ccb2f56d822cd399ae357
Dan Thiffault <dan.thiffault@bookbub.com>
2017-02-19 21:22:06 -0500

    Updated script for ansible

--- scripts/bootstrap_ansible.sh
+++ scripts/bootstrap_ansible.sh
@@ -1,2 +1,7 @@
 # /bin/sh
-touch /tmp/foo
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install git ansible awscli -y
+git clone https://github.com/1nterrupt/terraform
+# aws s3 cp s3://1nterrupt-util/terraform.tfvars terraform/
+
